d7e21973922a4cb2a9b556e07e44c80ef9dde870
Maradiaga05623 <hdmaradiaga@unah.hn>
2020-05-07 23:31:01 -0600

    Endpoint Update
    ahora se pueden actualizar registros de pacientes
    Saludos Cordiales

--- restfulapi-typeorm-uroanalisis/src/controller/paciente.controller.ts
+++ restfulapi-typeorm-uroanalisis/src/controller/paciente.controller.ts
@@ -11,5 +11,7 @@ export class PacienteController{
     private routes(){
 
         this.app.route("/pacientes").get(this.paciente_service.getAll);
+
+        this.app.route("/paciente/:id").put(this.paciente_service.updateOne);
     }
 }
--- restfulapi-typeorm-uroanalisis/src/services/paciente.service.ts
+++ restfulapi-typeorm-uroanalisis/src/services/paciente.service.ts
@@ -10,7 +10,44 @@ export class PacienteService{
         const paciente = await getConnection().getRepository(Paciente).find();
         res.status(200).json(paciente);
     }
+    public async updateOne(req:Request, res: Response){
+        try{
+    
+            await getConnection().createQueryBuilder().update(Paciente)
+            .set({
+                PrimerNombre:req.body.PrimerNombre,
+                SegundoNombre:req.body.SegundoNombre,
+                PrimerApellido:req.body.PrimerApellido,
+                SegundoApellido:req.body.SegundoApellido,
+                Genero:req.body.Genero,
+                FechaNacimiento:req.body.FechaNacimiento,
+                EstadoCivil:req.body.EstadoCivil,
+                Direccion:req.body.Direccion,
+                Email:req.body.Email,
+                Telefono:req.body.Telefono,
+                Observaciones:req.body.Observaciones,
 
+               
+            })
+            .where("PacienteID = :id",{id: req.params.id})
+            .execute();
+    
+            res.status(200).json({
+                updated: true
+            });
+    
+    
+        }catch(Error){
+            res.status(401).json({
+                updated: false,
+                Message: Error.Message
+            });
 } 
+
+
+   }
+   
+}
+
 
 
