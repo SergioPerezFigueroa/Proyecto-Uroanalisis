677fa80608ca23b95b01bae52f418db62878a9df
Usagi Tsukino <58183300+Johan-Vasquez@users.noreply.github.com>
2020-05-11 03:53:20 -0600

    UpdatePreclinica

--- restfulapi-typeorm-uroanalisis/src/controller/PreClinica.controller.ts
+++ restfulapi-typeorm-uroanalisis/src/controller/PreClinica.controller.ts
@@ -9,7 +9,10 @@ export class PreClinicaController{
         this.routes();
     }
     private routes(){
-        this.app.route("/preclinica/:ID").get(this.preclinica_service.getOne);
+
+        this.app.route("/preclinica/:ID")
+        .get(this.preclinica_service.getOne)
+        .put(this.preclinica_service.updateOne);
         this.app.route("/preclinica").get(this.preclinica_service.getAll);
     }
 }
--- restfulapi-typeorm-uroanalisis/src/services/PreClinica.service.ts
+++ restfulapi-typeorm-uroanalisis/src/services/PreClinica.service.ts
@@ -13,6 +13,39 @@ export class PreClinicaService{
         const preclinica : PreClinica [] = await getConnection().getRepository(PreClinica).find( {where : { PreClinicaID: req.params.ID } });
         res.status(200).json(preclinica[0]);
     }
+
+    public async updateOne(req: Request, res: Response){
+        try{
+        
+        await getConnection().createQueryBuilder().update(PreClinica)
+        .set({
+            
+            PacienteID: req.body.PacienteID,
+            EnfermeroID: req.body.EnfermeroID,
+            MedicoID: req.body.MedicoID,
+            Fecha: req.body.Fecha,
+            Presion: req.body.Presion,
+            Altura: req.body.Altura,
+            Peso: req.body.Peso,
+            Temperatura: req.body.Temperatura
+
+        })
+        .where("PreClinicaID=:ID",{ID: req.params.ID})
+        .execute();
+
+        res.status(200).json({
+            updated:true            
+        });
+
+        }catch(Error){
+            res.status(402).json({
+                update: false,
+                Message: Error.Message
+            });
+
+        }
+
+    }
 }    
 
 
